23e28d27398579d05c1e9f4b2f74e5d180b9b1b2
diablo2532 <nikitos2532@gmail.com>
2026-07-25 21:07:37 +0200

    Add all answer for task

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,20 @@
+// Температура повітря 
+let intTempe = 24 + '°' ;
+console.log('Air temperature:', intTempe); 
+// Назва книжки
+let nameBook;
+console.log('Book name:', nameBook);
+// Довжина 
+let length = 3 ;
+// Ширина
+let width = 4 ; 
+// площа трикутника
+const areaRectangle = (length * width);
+console.log('Rectangle area:', areaRectangle);
+// Чи є фігура трикутником 
+let tringleShape = false ;
+console.log('Tringle Shape:', tringleShape); 
+// Брак елементу
+let element = null; 
+console.log('Element:', element)
 
