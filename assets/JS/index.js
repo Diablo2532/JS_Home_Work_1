14e0639fd61d9c4cac8026ffb52fc8549f9fdafe
diablo2532 <nikitos2532@gmail.com>
2026-07-25 22:08:00 +0200

    Complite last task

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,18 @@
-
+// 1
+let firstNumber = Number(prompt ('Введіть число:'));
+let square = firstNumber ** 2;
+alert('Квадрат числа :' + square);
+// 2
+let numberArf1 = Number(prompt('Введіть перше число:'));
+let numberArf2 = Number(prompt('Введіть друге число:'));
+let aritchmeticMean = (numberArf1 + numberArf2)/2 ; 
+alert('Середє арифметичне :' + aritchmeticMean);
+// 3
+let minNum = Number(prompt('Подайте кількість хвилин :')) 
+let secondsNum = minNum * 60 ; 
+alert('Кількість в секундах :'+ secondsNum);
+// 4
+let greeting = 'Hello';
+let userName = prompt('Веддіть свое імя :');
+let sumName = greeting + ',' + userName + '!';
+alert(sumName);
