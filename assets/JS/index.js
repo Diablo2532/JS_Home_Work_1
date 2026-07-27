6ceb3927848e2e806afdc8a3ccb65d172fbbbbfb
diablo2532 <nikitos2532@gmail.com>
2026-07-27 20:41:45 +0200

    Edit code for mistacks

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,44 +1,44 @@
 
 // Температура повітря 
-let intTempe = 24 + '°' ;
-console.log('Air temperature:', intTempe); 
+const intTempe = 24 ;
+console.log('Air temperature:', intTempe, '°'); 
 // Назва книжки
-let nameBook;
+const nameBook;
 console.log('Book name:', nameBook);
 // Довжина 
-let length = 3 ;
+const length = 3 ;
 // Ширина
-let width = 4 ; 
+const width = 4 ; 
 // площа трикутника
 const areaRectangle = (length * width);
 console.log('Rectangle area:', areaRectangle);
 // Чи є фігура трикутником 
-let tringleShape = false ;
+const tringleShape = false ;
 console.log('Tringle Shape:', tringleShape); 
 // Брак елементу
-let element = null; 
+const element = null; 
 console.log('Element:', element)
 
 // множеня двух елементив 
-let firstVar = 3 ;
-let secondVar = 10 ; 
+const firstVar = 3 ;
+const secondVar = 10 ; 
 const resultMul = firstVar * secondVar ;
 console.log('Result of multiplication :', resultMul); 
 // ділення 
-let firstVar_1 = 3 ;
-let secondVar_1 = 10 ; 
-const resultDiv = firstVar/secondVar ;
+const firstVar_1 = 3 ;
+const secondVar_1 = 10 ; 
+const resultDiv = firstVar_1/secondVar_1 ;
 console.log('Result of division :', resultDiv);
 //  Cумма 
-let firstVar_2 = 3 ;
-let secondVar_2 = 10 ; 
-const resultSumm = firstVar + secondVar ;
+const firstVar_2 = 3 ;
+const secondVar_2 = 10 ; 
+const resultSumm = firstVar_2 + secondVar_2 ;
 console.log('Result of summ :', resultSumm);
 // 4 
-let number = 11 ; 
-let bool = true ; 
-let string = 'java script';
-let stringNumber = '100';
+const number = 11 ; 
+const bool = true ; 
+const string = 'java script';
+const stringNumber = '100';
 
 console.log('Number:', number);
 console.log('Bool :', bool);
@@ -60,20 +60,20 @@ num--;
 console.log('num -1 :', num);
 // 1
 let firstNumber = Number(prompt ('Введіть число:'));
-let square = firstNumber ** 2;
+const square = firstNumber ** 2;
 alert('Квадрат числа :' + square);
 // 2
 let numberArf1 = Number(prompt('Введіть перше число:'));
 let numberArf2 = Number(prompt('Введіть друге число:'));
-let aritchmeticMean = (numberArf1 + numberArf2)/2 ; 
+const aritchmeticMean = (numberArf1 + numberArf2)/2 ; 
 alert('Середє арифметичне :' + aritchmeticMean);
 // 3
 let minNum = Number(prompt('Подайте кількість хвилин :')) 
-let secondsNum = minNum * 60 ; 
+const secondsNum = minNum * 60 ; 
 alert('Кількість в секундах :'+ secondsNum);
 // 4
-let greeting = 'Hello';
+const greeting = 'Hello';
 let userName = prompt('Веддіть свое імя :');
-let sumName = greeting + ',' + userName + '!';
+const sumName = greeting + ',' + userName + '!';
 alert(sumName);
 
